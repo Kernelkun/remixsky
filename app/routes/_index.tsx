186fbb2e4d13b42d7fb5445a76d80f8f0554eae7
José Juan Gázquez <jose.j.gazquez@gmail.com>
2024-11-18 17:52:21 +0100

    feat: Viewing a post

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { agent } from "~/lib/api";
 import { useLoaderData } from "@remix-run/react";
+import { AppBskyFeedDefs, AppBskyFeedPost } from "@atproto/api";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,24 +11,47 @@ export const meta: MetaFunction = () => {
 };
 
 export async function loader() {
-  const feeds = await agent.app.bsky.unspecced.getPopularFeedGenerators({
-    limit: 10,
+  const EXAMPLE_POST =
+    "at://did:plc:vwzwgnygau7ed7b7wt5ux7y2/app.bsky.feed.post/3karfx5vrvv23";
+  const thread = await agent.app.bsky.feed.getPostThread({
+    uri: EXAMPLE_POST,
   });
 
-  return { feeds };
+  if (!AppBskyFeedDefs.isThreadViewPost(thread.data.thread))
+    throw new Error("Expected a thread view post");
+
+  const post = thread.data.thread.post;
+
+  if (!AppBskyFeedPost.isRecord(post.record))
+    throw new Error("Expected a post with a record");
+
+  return { post, thread };
 }
 
 export default function Index() {
-  const { feeds } = useLoaderData<typeof loader>();
-  return (
-    <div className="container mx-auto">
-      <h1 className="font-bold text-xl my-4">Top Feeds</h1>
+  const { post } = useLoaderData<typeof loader>();
 
-      <ul>
-        {feeds.data.feeds.map((feed) => (
-          <li key={feed.displayName}>{feed.displayName}</li>
-        ))}
-      </ul>
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-row items-center">
+          <img
+            src={post.author.avatar}
+            alt={post.author.handle}
+            className="h-12 w-12 rounded-full"
+          />
+
+          <div className="ml-4">
+            <p className="text-lg font-medium">{post.author.displayName}</p>
+
+            <p>@{post.author.handle}</p>
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <p>{post.record.text}</p>
+        </div>
+      </div>
     </div>
   );
 }
