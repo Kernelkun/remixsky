0709796b4e284db31f802d14ca1623e2820d7231
José Juan Gázquez <jose.j.gazquez@gmail.com>
2024-11-18 19:08:21 +0100

    feat: Viewing any post

---
+++ app/routes/profile.$handle.post.$rkey.tsx
@@ -0,0 +1,57 @@
+import { LoaderFunctionArgs } from "@remix-run/router";
+import { useLoaderData } from "@remix-run/react";
+import { agent } from "~/lib/api";
+import { AppBskyFeedDefs, AppBskyFeedPost } from "@atproto/api";
+
+type Params = {
+  handle: string;
+  rkey: string;
+};
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const typedParams = params as Params;
+  const uri = `at://${decodeURIComponent(typedParams.handle)}/app.bsky.feed.post/${
+    typedParams.rkey
+  }`;
+
+  const thread = await agent.app.bsky.feed.getPostThread({
+    uri: uri,
+  });
+
+  if (!AppBskyFeedDefs.isThreadViewPost(thread.data.thread))
+    throw new Error("Expected a thread view post");
+
+  const post = thread.data.thread.post;
+
+  if (!AppBskyFeedPost.isRecord(post.record))
+    throw new Error("Expected a post with a record");
+
+  return { post, thread };
+}
+export default function PostView() {
+  const { post } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-row items-center">
+          <img
+            src={post.author.avatar}
+            alt={post.author.handle}
+            className="h-12 w-12 rounded-full"
+          />
+
+          <div className="ml-4">
+            <p className="text-lg font-medium">{post.author.displayName}</p>
+
+            <p>@{post.author.handle}</p>
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <p>{post.record.text}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
