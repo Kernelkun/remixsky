d7cd3b1514682bc98bd9d6feaab688b61996c9c2
José Juan Gázquez <jose.j.gazquez@gmail.com>
2024-11-20 20:11:07 +0100

    ix: type error

--- app/root.tsx
+++ app/root.tsx
@@ -8,19 +8,21 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+import type { LinkDescriptor } from "@remix-run/server-runtime/dist/links";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: LinksFunction = () =>
+  [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "anonymous",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    },
+  ] as LinkDescriptor[];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
